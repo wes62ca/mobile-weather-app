93a3983af490382087fd719240fd225ce7c5f088
wes62ca <weslleyezequiel32@gmail.com>
2023-05-01 03:58:59 -0300

    Main de informações

--- my-app/screens/Index.tsx
+++ my-app/screens/Index.tsx
@@ -1,57 +1,90 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import {MapPin,CaretDown,Bell} from "phosphor-react-native"
-import Sun from '../../assets 01d.svg';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { MapPin, CaretDown, Bell } from "phosphor-react-native";
+import RainSvg from "../../my-app/assets/rain.svg";
+
 export default function Home() {
   return (
-    <LinearGradient colors={["#0B42AB","#134CB5","#08244F"]} 
-    style={styles.container}>
-    
-    
-    <View style={styles.content}>
+    <LinearGradient
+      colors={["#0B42AB", "#134CB5", "#08244F"]}
+      style={styles.container}
+    >
+      <View style={styles.content}>
         <View style={styles.header}>
-            <View style={styles.headerLeft}>
-            <MapPin color='#fff' size={27} />
+          <View style={styles.headerLeft}>
+            <MapPin color="#FFF" size={27} />
             <Text style={styles.headerLeftText}>Arapiraca</Text>
-            <CaretDown color='#fff' size={27} />
-            </View> 
-            <Bell size={29} />
+            <CaretDown color="#FFF" size={27} />
+          </View>
+          <Bell color="#FFF" size={29} />
         </View>
-        <View style={styles.info} >
-          
-
+        <View style={styles.info}>
+        <RainSvg   width={284} height={207}
+         style={{
+          left: 19,
+          top: 35,
+        }}
+        />
+          <Text style={[styles.infoText, { marginBottom: 10 }]}>24°C</Text>
+          <Text style={styles.infoTextMaxMin}>Max.: 31º  Min.: 25º</Text>
         </View>
-    </View>
+        </View>
     </LinearGradient>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-
-    content:  { paddingHorizontal: 41,
-                paddingTop: 44,  
-    },
-    header:   {
-      width: "100%",
-      flexDirection: "row",
-      justifyContent: "space-between",
+  content: { 
+    paddingHorizontal: 41,
+    paddingTop: 44, 
+    alignItems: "center",
+    justifyContent: 'center',
+   },
 
 
-    }, 
-    headerLeft: {
-      flexDirection:"row",
-      alignItems: "center",
-      gap: 10,
-    },
+  
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
 
-    headerLeftText: {
-      color: "#fff",
-      fontSize: 18,
-      fontWeight: "600",
-    }
-  });
+  headerLeftText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+
+  infoText: {
+    fontSize: 64,
+    fontWeight: 600,
+    color: "#FFFFFF",
+
+  },
+
+  info:{
+    paddingVertical: 70,
+    alignItems: "center",
+    gap:6,
+
+  },
+
+  infoTextMaxMin:{
+    fontSize: 18,
+    fontWeight: 400,
+    color:"#FFFFFF"
+
+
+  },
+
+});
